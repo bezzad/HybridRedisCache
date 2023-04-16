960cd69859a43ba7b7a034dc9af6e650d0f37db6
bezzad <behzad.khosravifar@gmail.com>
2023-04-16 18:13:57 +0430

    add comment on public methods

--- src/HybridRedisCache/HybridCache.cs
+++ src/HybridRedisCache/HybridCache.cs
@@ -5,6 +5,10 @@ using System.Text.Json;
 
 namespace HybridRedisCache;
 
+/// <summary>
+/// The HybridCache class provides a hybrid caching solution that stores cached items in both
+/// an in-memory cache and a Redis cache. 
+/// </summary>
 public class HybridCache : IHybridCache, IDisposable
 {
     private readonly IMemoryCache _memoryCache;
@@ -15,6 +19,13 @@ public class HybridCache : IHybridCache, IDisposable
     private readonly ISubscriber _redisSubscriber;
     private string InvalidationChannel => _instanceName + ":invalidate";
 
+    /// <summary>
+    /// This method initializes the HybridCache instance and subscribes to Redis key-space events 
+    /// to invalidate cache entries on all instances. 
+    /// </summary>
+    /// <param name="redisConnectionString">Redis connection string</param>
+    /// <param name="instanceName">Application unique name for redis indexes</param>
+    /// <param name="defaultExpiryTime">default caching expiry time</param>
     public HybridCache(string redisConnectionString, string instanceName, TimeSpan? defaultExpiryTime = null)
     {
         _memoryCache = new MemoryCache(new MemoryCacheOptions());
@@ -34,6 +45,14 @@ public class HybridCache : IHybridCache, IDisposable
         _memoryCache.Remove(cacheKey);
     }
 
+    /// <summary>
+    /// Sets a value in the cache with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to cache.</typeparam>
+    /// <param name="key">The cache key.</param>
+    /// <param name="value">The value to cache.</param>
+    /// <param name="expiration">The expiration time for the cache entry. If not specified, the default expiration time is used.</param>
+    /// <param name="fireAndForget">Whether to cache the value in Redis without waiting for the operation to complete.</param>
     public void Set<T>(string key, T value, TimeSpan? expiration = null, bool fireAndForget = true)
     {
         var cacheKey = GetCacheKey(key);
@@ -42,6 +61,12 @@ public class HybridCache : IHybridCache, IDisposable
             flags: fireAndForget ? CommandFlags.FireAndForget : CommandFlags.None);
     }
 
+    /// <summary>
+    /// Gets a cached value with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the cached value.</typeparam>
+    /// <param name="key">The cache key.</param>
+    /// <returns>The cached value, or null if the key is not found in the cache.</returns>
     public T Get<T>(string key)
     {
         var cacheKey = GetCacheKey(key);
@@ -61,6 +86,10 @@ public class HybridCache : IHybridCache, IDisposable
         return value;
     }
 
+    /// <summary>
+    /// Removes a cached value with the specified key.
+    /// </summary>
+    /// <param name="key">The cache key.</param>
     public void Remove(string key)
     {
         var cacheKey = GetCacheKey(key);
@@ -69,14 +98,29 @@ public class HybridCache : IHybridCache, IDisposable
         _redisDb.Publish(InvalidationChannel, cacheKey);
     }
 
+    /// <summary>
+    /// Asynchronously sets a value in the cache with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to cache.</typeparam>
+    /// <param name="key">The cache key.</param>
+    /// <param name="value">The value to cache.</param>
+    /// <param name="expiration">The expiration time for the cache entry. If not specified, the default expiration time is used.</param>
+    /// <param name="fireAndForget">Whether to cache the value in Redis without waiting for the operation to complete.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task SetAsync<T>(string key, T value, TimeSpan? expiration = null, bool fireAndForget = true)
     {
         var cacheKey = GetCacheKey(key);
         _memoryCache.Set(cacheKey, value, expiration ?? _defaultExpiration);
         await _redisDb.StringSetAsync(cacheKey, Serialize(value), expiration ?? _defaultExpiration,
-             flags: fireAndForget ? CommandFlags.FireAndForget : CommandFlags.None);
+            flags: fireAndForget ? CommandFlags.FireAndForget : CommandFlags.None);
     }
 
+    /// <summary>
+    /// Asynchronously gets a cached value with the specified key.
+    /// </summary>
+    /// <typeparam name="T">The type of the cached value.</typeparam>
+    /// <param name="key">The cache key.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the cached value, or null if the key is not found in the cache.</returns>
     public async Task<T> GetAsync<T>(string key)
     {
         var cacheKey = GetCacheKey(key);
@@ -96,6 +140,11 @@ public class HybridCache : IHybridCache, IDisposable
         return value;
     }
 
+    /// <summary>
+    /// Asynchronously removes a cached value with the specified key.
+    /// </summary>
+    /// <param name="key">The cache key.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task RemoveAsync(string key)
     {
         var cacheKey = GetCacheKey(key);
@@ -104,10 +153,7 @@ public class HybridCache : IHybridCache, IDisposable
         await _redisDb.PublishAsync(InvalidationChannel, cacheKey);
     }
 
-    private string GetCacheKey(string key)
-    {
-        return $"{_instanceName}:{key}";
-    }
+    private string GetCacheKey(string key) => $"{_instanceName}:{key}";
 
 
     private byte[] Serialize<T>(T value)
@@ -134,8 +180,8 @@ public class HybridCache : IHybridCache, IDisposable
 
     public void Dispose()
     {
-        _redisSubscriber.UnsubscribeAll();
-        _redisConnection.Dispose();
-        _memoryCache.Dispose();
+        _redisSubscriber?.UnsubscribeAll();
+        _redisConnection?.Dispose();
+        _memoryCache?.Dispose();
     }
 }
