ca52af4ff92a683a9dbacb8fcb1704da6b2f6b15
bezzad <behzad.khosravifar@gmail.com>
2023-04-17 15:37:25 +0330

    added new sample project

---
+++ src/HybridRedisCache.Sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using HybridRedisCache;
+
+Console.WriteLine("Welcome Hybrid Redis Cache");
+
+// Create a new instance of HybridCache with cache options
+var options = new HybridCachingOptions()
+{
+    DefaultExpirationTime = TimeSpan.FromSeconds(1),
+    InstanceName = "SampleApp",
+    ThrowIfDistributedCacheError = true,
+    RedisCacheConnectString = "localhost:6379"
+};
+var cache = new HybridCache(options);
+
+Console.WriteLine($"retrieving 'mykey' value from redis cache...");
+var retrivedValue = cache.Get<string>("mykey");
+Console.WriteLine($"value is: {retrivedValue}\n\n");
+
+// Cache a string value with key "mykey" for 1 minute
+cache.Set("mykey", Guid.NewGuid().ToString("N"), TimeSpan.FromMinutes(100));
+Console.WriteLine($"mykey cached with a new GUID value for 100min");
+
+// Retrieve the cached value with key "mykey"
+Console.WriteLine($"retrieving mykey value from local memory cache...");
+retrivedValue = cache.Get<string>("mykey");
+Console.WriteLine($"value is: {retrivedValue}");
