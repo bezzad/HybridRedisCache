6d8194ecbd35a871eca8801853a4540041c5becb
bezzad <behzad.khosravifar@gmail.com>
2023-05-16 15:54:20 +0330

    fixed load test api

--- src/HybirdRedisCache.Sample.WebAPI/Controllers/WeatherForecastController.cs
+++ src/HybirdRedisCache.Sample.WebAPI/Controllers/WeatherForecastController.cs
@@ -22,56 +22,32 @@ public class WeatherForecastController : ControllerBase
         _cacheService = cacheService;
     }
 
-    [HttpGet]
-    public async Task<WeatherForecast[]> GetAll([FromQuery] int count = 100)
-    {
-        _logger.LogInformation($"GET All: WeatherForecast[count: {count}]");
-        var cacheData = GetKeyValues(count);
-        if (cacheData.Any())
-        {
-            return cacheData;
-        }
-
-        var newData = Enumerable.Range(1, 100).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        }).ToArray();
-
-        foreach (var data in newData)
-            await Save(data).ConfigureAwait(false);
-
-        return newData;
-    }
-
     [HttpGet("{id}")]
     public Task<WeatherForecast> Get(string id)
     {
-        _logger.LogInformation($"GET: WeatherForecast {{ id: {id} }}");
-        _cacheService.TryGetValue<WeatherForecast>(GetKey(id), out var filteredData);
-        return Task.FromResult(filteredData);
+        //_logger.LogInformation($"GET: WeatherForecast {{ id: {id} }}");
+        return _cacheService.GetAsync<WeatherForecast>(GetKey(id));
     }
 
     [HttpPost]
     public async Task Post(WeatherForecast value)
     {
-        _logger.LogInformation($"Post: WeatherForecast {{ id: {value.Id} }}");
+        //_logger.LogInformation($"Post: WeatherForecast {{ id: {value.Id} }}");
         await Save(value).ConfigureAwait(false);
     }
 
     [HttpDelete("{id}")]
     public async Task Delete(string id)
     {
-        _logger.LogInformation($"Delete: WeatherForecast {{ id: {id} }}");
+        //_logger.LogInformation($"Delete: WeatherForecast {{ id: {id} }}");
         await _cacheService.RemoveAsync(GetKey(id)).ConfigureAwait(false);
     }
 
     [HttpDelete("ClearAll")]
     public async Task Delete()
     {
-        _logger.LogInformation($"Delete: all WeatherForecast");
-        await _cacheService.ClearAllAsync();
+        //_logger.LogInformation($"Delete: all WeatherForecast");
+        await _cacheService.ClearAllAsync().ConfigureAwait(false);
     }
 
     private async Task<bool> Save(WeatherForecast weather)
@@ -79,16 +55,6 @@ public class WeatherForecastController : ControllerBase
         var expirationTime = TimeSpan.FromMinutes(120);
         await _cacheService.SetAsync(GetKey(weather.Id), weather, expirationTime, fireAndForget: false).ConfigureAwait(false);
         return true;
-    }
-
-    private WeatherForecast[] GetKeyValues(int count = 100)
-    {
-        var result = new List<WeatherForecast>();
-        for (int i = 0; i < count; i++)
-            if (_cacheService.TryGetValue<WeatherForecast>(GetKey(i.ToString()), out var data))
-                result.Add(data);
-
-        return result.ToArray();
     }
 
     private string GetKey(string id)
--- src/HybridRedisCache.LoadTest/app.js
+++ src/HybridRedisCache.LoadTest/app.js
@@ -20,13 +20,14 @@ export const options = {
     // Ramp the number of virtual users up and down
     stages: [
         { target: 500, duration: '30s' },  // linearly go from 5 iters/s to 500 iters/s for 30s
-        { target: 800, duration: '5s' },   // linearly go from 500 iters/s to 800 iters/s for 5s
-        { target: 1000, duration: '0' },   // instantly jump to 1000 iters/s
-        { target: 1000, duration: '2m' },  // continue with 1000 iters/s for 2 minutes
+        { target: 1000, duration: '5s' },   // linearly go from 500 iters/s to 1000 iters/s for 5s
+        { target: 1500, duration: '30s' },   // linearly go from 1000 iters/s to 1500 iters/s for 30s
+        { target: 2000, duration: '0' },   // instantly jump to 2000 iters/s
+        { target: 2000, duration: '2m' },  // continue with 2000 iters/s for 2 minutes
     ],
     thresholds: {
         // Assert that 99% of requests finish within 1000ms.
-        http_req_duration: ["p(99) < 400"],
+        http_req_duration: ["p(99) < 1000"],
     },
     noConnectionReuse: true,
     userAgent: 'HybridRedisCache_K6_LoadTest/1.0',
@@ -35,6 +36,21 @@ export const options = {
 
 // Simulated user behavior
 export default function (authToken) {
+
+    let payload = {
+        id: uuidv4(),
+        date: new Date(),
+        temperatureC: randomIntBetween(1, 40),
+        summary: `Name ${randomString(10)}`
+    }
+
+    let newPayload = {
+        id: payload.id,
+        date: payload.date,
+        temperatureC: 22,
+        summary: "new summery"
+    };
+
     var requestConfigWithTag = (tag) => ({
         'headers': {
             //Authorization: `Bearer ${authToken}`,
@@ -49,88 +65,77 @@ export default function (authToken) {
         ),
     });
 
-    var getSampleWeather = () => ({
-        id: uuidv4(),
-        date: new Date(),
-        temperatureC: randomIntBetween(1, 40),
-        summary: `Name ${randomString(10)}`
+    group('Create a weather', () => {
+        let res = http.post(BASE_URL, JSON.stringify(payload), requestConfigWithTag({ name: 'Create' }));
+        myTrend.add(res.timings.waiting);
+        if (!check(res, { 'Weather created correctly': (r) => r.status === 200 })) {
+            console.error(`Unable to create a Weather ${res.status} ${res.body}`);
+        }
     });
 
-    group('Create and modify weathers', () => {
-        let payload = getSampleWeather();
-        let newPayload = getSampleWeather();
-        newPayload.id = payload.id;
+    sleep(1);
 
-        group('Create weathers', () => {
-            let res = http.post(BASE_URL, JSON.stringify(payload), requestConfigWithTag({ name: 'Create' }));
-            myTrend.add(res.timings.waiting);
-            if (!check(res, { 'Weather created correctly': (r) => r.status === 200 })) {
-                console.error(`Unable to create a Weather ${res.status} ${res.body}`);
-            }
-        });
+    //group('GET with 10 aggregate requests', () => {
+    //    let responses = http.batch([
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //        ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
+    //    ]);
 
-        group('GET with 10 aggregate requests', () => {
-            let responses = http.batch([
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-                ['GET', `${BASE_URL}/${payload.id}`, requestConfigWithTag({ name: 'GET' })],
-            ]);
+    //    const temperatures = Object.values(responses).map((res) => res.json('temperatureC'));
+    //    if (!check(temperatures, {
+    //        'Weathers temperatures are more than 1°C': Math.min(...temperatures) >= 1,
+    //        'Weathers temperatures are less than 40°C': Math.max(...temperatures) <= 40,
+    //    })) {
+    //        console.error(`Unable to get the Weather(id: ${payload.id})! ${res.status} ${res.body}`);
+    //    };
+    //});
+    sleep(1);
 
-            const temperatures = Object.values(responses).map((res) => res.json('temperatureC'));
 
-            // Functional test
-            if (!check(temperatures, {
-                'Weathers temperatures are more than 1°C': Math.min(...temperatures) >= 1,
-                'Weathers temperatures are less than 40°C': Math.max(...temperatures) <= 40,
-            })) {
-                console.error(`Unable to get the Weather(id: ${payload.id}): ${res.status} ${res.body}`);
-            };
-        });
+    group('Update weather', () => {
+        let res = http.post(BASE_URL, JSON.stringify(newPayload), requestConfigWithTag({ name: 'Update' }));
+        myTrend.add(res.timings.waiting);
+        if (!check(res, { 'Update worked': (r) => r.status == 200 })) {
+            console.error(`Unable to update the weather(id: ${newPayload.id})! ${res.status} ${res.body}`);
+        }
+    });
 
-        group('Update weather', () => {
-            let res = http.post(BASE_URL, JSON.stringify(newPayload), requestConfigWithTag({ name: 'Update' }));
-            myTrend.add(res.timings.waiting);
-            if (!check(res, { 'Update worked': (r) => r.status == 200 })) {
-                console.error(`Unable to update the weather(id: ${newPayload.id}): ${res.status} ${res.body}`);
-            }
-        });
+    sleep(1);
 
-        group('Read and verify updated data', () => {
-            let res = http.get(`${BASE_URL}/${newPayload.id}`, requestConfigWithTag({ name: 'Read and Verify' }));
-            myTrend.add(res.timings.waiting);
-            if (!check(res, {
-                'Update worked': (r) => r.status === 200,
-                'Updated name is correct': (r) => JSON.parse(r.body).summary == newPayload.summary,
-                'Updated temp is correct': (r) => JSON.parse(r.body).temperatureC == newPayload.temperatureC,
-            })) {
-                console.error(`Verify update the weather(id: ${newPayload.id}) was unsuccess! ${res.status} ${res.body}`);
-                console.error(res);
-            }
-        });
+    group('Read and verify updated data', () => {
+        let res = http.get(`${BASE_URL}/${newPayload.id}`, requestConfigWithTag({ name: 'Read and Verify' }));
+        myTrend.add(res.timings.waiting);
+        if (!check(res, {
+            'Update worked': (r) => r.status === 200,
+            'Updated name is correct': (r) => JSON.parse(r.body).summary == newPayload.summary,
+            'Updated temp is correct': (r) => JSON.parse(r.body).temperatureC == newPayload.temperatureC,
+        })) {
+            console.error(`Verify update the weather(id: ${newPayload.id}) was unsuccess! ${res.status} ${res.body}`);
+        }
+    });
 
-        group('Delete weather', () => {
-            let res = http.del(`${BASE_URL}/${newPayload.id}`, null, requestConfigWithTag({ name: 'Delete' }));
-            myTrend.add(res.timings.waiting);
-            if (!check(res, { 'Weather was deleted correctly': () => res.status === 200 })) {
-                console.error(`Weather(id: ${newPayload.id}) was not deleted properly`);
-            }
-        });
+    sleep(1);
+
+    group('Delete weather', () => {
+        let res = http.del(`${BASE_URL}/${newPayload.id}`, null, requestConfigWithTag({ name: 'Delete' }));
+        myTrend.add(res.timings.waiting);
+        if (!check(res, { 'Weather was deleted correctly': () => res.status === 200 })) {
+            console.error(`Weather(id: ${newPayload.id}) was not deleted properly! ${res.status} ${res.body}`);
+        }
     });
 
     sleep(1);
 }
 
 export function handleSummary(data) {
-    const med_latency = data.metrics.iteration_duration.values.med;
-    const latency_message = `The median latency was ${med_latency}\n`;
-
     return {
         // stdout: latency_message,
         'summary.json': JSON.stringify(data), //the default data object
