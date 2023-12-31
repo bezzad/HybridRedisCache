3d5434ba3dc0be453dc65f173e26ce6dfe256e3a
bezzad <behzad.khosravifar@gmail.com>
2023-12-31 16:04:41 +0330

    refactor HybridCacheTimeoutAndRetryTests

--- src/HybridRedisCache.Test/HybridCacheTimeoutAndRetryTests.cs
+++ src/HybridRedisCache.Test/HybridCacheTimeoutAndRetryTests.cs
@@ -5,33 +5,45 @@ namespace HybridRedisCache.Test;
 
 public class HybridCacheTimeoutAndRetryTests
 {
-    private readonly HybridCachingOptions _cachingOptions = new()
+    private HybridCachingOptions GetOption()
     {
-        InstancesSharedName = "my-test-app",
-        RedisConnectString = "localhost:9379", //Invalid Redis Connection (On Purpose)
-        ThrowIfDistributedCacheError = true,
-        AbortOnConnectFail = true,
-        ConnectRetry = 4,
-        FlushLocalCacheOnBusReconnection = false,
-        ConnectionTimeout = 1000,
-        SyncTimeout = 1000,
-        AsyncTimeout = 1000
-    };
+        return new HybridCachingOptions()
+        {
+            InstancesSharedName = "my-test-app",
+            RedisConnectString = "localhost:9379", //Invalid Redis Connection (On Purpose)
+            ThrowIfDistributedCacheError = true,
+            AbortOnConnectFail = true,
+            ConnectRetry = 4,
+            FlushLocalCacheOnBusReconnection = false,
+            ConnectionTimeout = 1000,
+            SyncTimeout = 1000,
+            AsyncTimeout = 1000
+        };
+    }
 
     [Fact]
     public void Should_Retry_On_ConnectionFailure()
     {
+        // arrange 
+        var delayPerRetry = 1000;
+        var retryCount = 3;
+        var opt = GetOption();
+        opt.ConnectionTimeout = delayPerRetry;
+        opt.ConnectRetry = retryCount;
+        
+        // act
         var stopWatch = Stopwatch.StartNew();
 
         try
         {
-            var cache = new HybridCache(_cachingOptions, new LoggerFactoryMock());
+            var cache = new HybridCache(opt, new LoggerFactoryMock());
         }
         catch
         {
             stopWatch.Stop();
         }
 
-        Assert.True(stopWatch.ElapsedMilliseconds >= 2000, $"Actual value {stopWatch.ElapsedMilliseconds}");
+        // assert
+        Assert.True(stopWatch.ElapsedMilliseconds >= delayPerRetry * retryCount, $"Actual value {stopWatch.ElapsedMilliseconds}");
     }
 }
